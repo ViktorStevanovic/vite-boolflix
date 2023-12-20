28428834754ac6507d229190d5ae78ec909bd6c8
ViktorStevanovic <stevanovic.lavoro@gmail.com>
2023-12-20 16:44:06 +0100

    implemented search movie function via api

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -3,16 +3,43 @@
         <div class="container">
             <a class="navbar-brand">Navbar</a>
             <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-danger" type="submit">Search</button>
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" v-model="movieName" @keyup.enter='changeMovie'>
+            <button class="btn btn-outline-danger" type="button" @click='changeMovie'>Search</button>
             </form>
         </div>
 </nav>
 </template>
 
 <script>
+import { store } from '../js/store.js';
+import axios from 'axios';
+
+
 export default {
     name: 'AppSearch',
+    data() {
+        return {
+            store,
+            movieName:'',
+        }
+    },
+    methods: {
+        changeMovie(){
+        this.getMovie('https://api.themoviedb.org/3/search/movie?api_key=92d62557ab1a829ebff768d97ab5dc74&query=' + this.movieName)
+      },
+        getMovie(url = 'https://api.themoviedb.org/3/search/movie?api_key=92d62557ab1a829ebff768d97ab5dc74&query='){
+            axios.get(url)
+            .then((response) => {
+            // handle success
+            console.log(response.data.results);
+            this.store.moviesList = response.data.results;
+            })
+            .catch(function (error) {
+            // handle error
+            console.error(error);
+            });
+        },
+    },
 }
 </script>
 <style lang="scss" scoped>
