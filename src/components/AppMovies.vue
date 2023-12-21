cc1900607f24541af9a69a464b9b3ae1b279161c
ViktorStevanovic <stevanovic.lavoro@gmail.com>
2023-12-21 10:23:00 +0100

    installed and added a flag npm for the language

--- src/components/AppMovies.vue
+++ src/components/AppMovies.vue
@@ -3,19 +3,29 @@
     <div v-for='movie in this.store.moviesList'>
         <h1>Title: {{ movie.title}}</h1>
         <p>Original title: {{movie.original_title}}</p>
-        <p>Language: {{movie.original_language}}</p>
+        <!-- <country-flag :country='movie.original_language'/> -->
+        <lang-flag :iso='movie.original_language'/>
         <p>Feedback: {{movie.vote_average}}</p>
+        
     </div>
+    
 </template>
 <script>
 import { store } from '../js/store.js';
+// import CountryFlag from '../../node_modules/vue-country-flag-next'
+import LangFlag from '../../node_modules/vue-lang-code-flags'
+
 export default {
     name: 'AppMovies',
     data() {
         return {
-            store
+            store,
+            
         }
     },
+    components: {
+                LangFlag
+            }
 }
 </script>
 <style lang="scss" scoped>
